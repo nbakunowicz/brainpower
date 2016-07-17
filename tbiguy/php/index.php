df617a95f5b7948b660751cc746305b8746c1104
nbakunowicz <nbakunowicz@gmail.com>
2016-07-17 14:55:49 -0500

    Adding index.php

---
+++ tbiguy/php/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<script type='text/javascript' src='/TBI/js/jquery-2.1.4.min.js'></script>
+		<script type='text/javascript' src='/TBI/js/bootstrap.js'></script>
+		<link rel='stylesheet' type='text/css' href='/TBI/css/bootstrap.css' />
+		<link rel='stylesheet' type='text/css' href='/TBI/css/bootstrap-theme.css' />
+		<link rel='stylesheet' type='text/css' href='/TBI/css/fontawesome.min.css' />
+		<link rel='stylesheet' type='text/css' href='/TBI/css/custom.css' />
+		
+	</head>
+	<body>
+		<nav class="navbar navbar-default">
+			<div class="col-xs-12 noPadding" style="height:20vh">
+			</div>
+			<div class="container-fluid">
+				<div class="collapse navbar-collapse" >
+					<ul class="nav nav-tabs navBarText navBarTabBorder">
+						<li id="aboutTbiTab">
+	        				<a href="#">About TBI</a>
+	        			</li>
+				        <li id="peopleWithTbiTab">
+				        	<a href="#">People With TBI</a>
+				        </li>
+				        <li id="professionalTab" class="dropdown">
+				        	<a href="#">Professional Resources</a>
+				        </li>
+				        <li id="communityTab" class="dropdown">
+				        	<a href="#">Community</a>
+				        </li>
+				        <li id="myBrainAndMeTab" class="dropdown">
+				        	<a href="#">My Brain & Me</a>
+				        </li>
+					</ul>
+				</div><!-- /.navbar-collapse -->
+			</div><!-- /.container-fluid -->
+		</nav>
+		<?php echo "Hello World!"?>
+	</body>
+</html>
