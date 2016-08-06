704db386ad5bd92bfd6ebce578e22a15c8c45ec1
gottebag <gabriel.gottfredsen@gmail.com>
2016-08-05 20:49:39 -0500

    updated paths

--- tbiguy/php/header.php
+++ tbiguy/php/header.php
@@ -3,10 +3,10 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<script type='text/javascript' src='/TBI/js/jquery-2.1.4.min.js'></script>
-		<script type='text/javascript' src='/TBI/js/bootstrap.js'></script>
-		<script type='text/javascript' src='/TBI/js/global.js'></script>
-		<link rel='stylesheet' type='text/css' href='/TBI/css/bootstrap.css' />
-		<link rel='stylesheet' type='text/css' href='/TBI/css/bootstrap-theme.css' />
-		<link rel='stylesheet' type='text/css' href='/TBI/css/fontawesome.min.css' />
-		<link rel='stylesheet' type='text/css' href='/TBI/css/custom.css' />
+		<script type='text/javascript' src='../js/jquery-2.1.4.min.js'></script>
+		<script type='text/javascript' src='../js/bootstrap.js'></script>
+		<script type='text/javascript' src='../js/global.js'></script>
+		<link rel='stylesheet' type='text/css' href='../css/bootstrap.css' />
+		<link rel='stylesheet' type='text/css' href='../css/bootstrap-theme.css' />
+		<link rel='stylesheet' type='text/css' href='../css/fontawesome.min.css' />
+		<link rel='stylesheet' type='text/css' href='../css/custom.css' />
--- tbiguy/php/navigationBar.php
+++ tbiguy/php/navigationBar.php
@@ -5,19 +5,19 @@
 		<div class="container-fullwidth">
 			<ul class="nav nav-tabs navBarText navBarTabBorder">
 				<li id="aboutTBITab" style="padding-left: 20px;">
-					<a href="/TBI/php/aboutTBI.php">About TBI</a>
+					<a href="../php/aboutTBI.php">About TBI</a>
 				</li>
 				<li id="peopleWithTBITab">
-					<a href="/TBI/php/peopleWithTBI.php">People With TBI</a>
+					<a href="../php/peopleWithTBI.php">People With TBI</a>
 				</li>
 				<li id="professionalResourcesTab" class="dropdown">
-					<a href="/TBI/php/professionalResources.php">Professional Resources</a>
+					<a href="../php/professionalResources.php">Professional Resources</a>
 				</li>
 				<li id="communityTab" class="dropdown">
-					<a href="/TBI/php/community.php">Community</a>
+					<a href="../php/community.php">Community</a>
 				</li>
 				<li id="myBrainAndMeTab" class="dropdown">
-					<a href="/TBI/php/myBrainAndMe.php">My Brain & Me</a>
+					<a href="../php/myBrainAndMe.php">My Brain & Me</a>
 				</li>
 			</ul>
 		</div>
