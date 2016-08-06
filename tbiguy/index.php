b79ed333d5b131770cf9e50826222374688ddaed
Nathan Yount <nathanyount314@gmail.com>
2016-08-05 22:39:52 -0500

    Home page styling

--- tbiguy/index.php
+++ tbiguy/index.php
@@ -19,7 +19,7 @@
     <div class="row">
         <?php include './php/sidebar.php'; ?>
         <div class="col-xs-10">
-            <h2>Home Page</h2>
+            <h2>About the TBI Guy's Neuroscience Hub</h2>
             Totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
             dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
             cor magni dolores
@@ -29,16 +29,40 @@
             Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex
             ea commodi consequatur?
 
-            <hr class="col-xs-12">
-
-            <h2>Hello World</h2>
-            Rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
+           	<div class="row">
+            <div class="col-xs-4">
+            <h2>Today in TBI</h2>
+            <img src="https://pbs.twimg.com/profile_images/251458608/brain_400x400.jpg">
+            <br><br>Rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
             dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
             cor magni dolores
             eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
             consectetur, adipisci velit,
             sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
             Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
+            </div>
+            <div class="col-xs-8">
+            <h2>Events in the Community</h2>
+            <img src="http://sunderlandcollege.ac.uk/app/uploads/2012/11/Big-Science-Event-Sector-Header.jpg">
+            <br><br>Rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
+            cor magni dolores
+            eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
+            consectetur, adipisci velit,
+            sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+            Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
+            </div>
+             <div class="col-xs-8">
+            <h2>My Brain & Me</h2>
+            <img src="http://files.ctctcdn.com/d1450562101/0f9d74cd-44dc-4267-80ea-f5e50628c5b8.jpg">
+            <br><br>Rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
+            cor magni dolores
+            eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
+            consectetur, adipisci velit,
+            sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+            Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
+            </div>
 
         </div>
     </div>
